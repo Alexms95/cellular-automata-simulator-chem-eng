b3a8bb78e75f513ab9789f17c483a6efe867f642
Alex Silva <alex.silva@radixeng.com.br>
2024-02-26 20:46:13 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/v1/exemplo', methods=['GET'])
+def get_exemplo():
+    """
+    Rota GET para obter um exemplo.
+    """
+    dados = {
+        "nome": "Exemplo",
+        "valor": 123
+    }
+    return jsonify(dados)
+
+if __name__ == '__main__':
+    app.run(debug=True)
