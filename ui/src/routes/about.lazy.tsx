55f59f7bb2a25f177281c1ba863510d309529b6f
Alex Silva <silva.alexmatos@outlook.com>
2024-10-10 21:17:06 -0300

    Add a test automata grid

--- ui/src/routes/about.lazy.tsx
+++ ui/src/routes/about.lazy.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { ReactP5Wrapper, Sketch } from "@p5-wrapper/react";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/about")({
@@ -6,18 +6,48 @@ export const Route = createLazyFileRoute("/about")({
 });
 
 function About() {
-  const { isPending, error, data, isFetching } = useQuery({
-    queryKey: [""],
-  });
+  const sketch: Sketch = (p5) => {
+    let grid: number[][];
+    const cols = 10;
+    const rows = cols;
+    const resolution = 50;
 
-  if (isPending) return "Loading...";
+    p5.setup = () => {
+      p5.createCanvas(500, 500);
+      p5.frameRate(5);
+      grid = createGrid();
+    };
 
-  if (error) return "An error has occurred: " + error.message;
+    p5.draw = () => {
+      p5.background(220);
+      for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+          const x = i * resolution;
+          const y = j * resolution;
+          if (grid[j][i] === 1) {
+            p5.fill("orange");
+            p5.stroke(220);
+            p5.rect(x, y, resolution - 1, resolution - 1);
+          } else {
+            p5.fill("blue");
+            p5.stroke(220);
+            p5.rect(x, y, resolution - 1, resolution - 1);
+          }
+        }
+      }
+      grid = createGrid();
+    };
+
+    function createGrid() {
+      return Array.from({ length: rows }).map(() =>
+        Array.from({ length: cols }).map(() => Math.floor(p5.random([0, 1])))
+      );
+    }
+  };
 
   return (
-    <div>
-      <div>{isFetching ? "Updating..." : ""}</div>
-      {JSON.stringify(data)}
+    <div className="flex justify-center">
+      <ReactP5Wrapper sketch={sketch} />
     </div>
   );
 }
