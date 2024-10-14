7b92100f6226b902a14f1ed510642ff85cd0060d
Alex Silva <silva.alexmatos@outlook.com>
2024-10-14 19:49:08 -0300

    Improving the p5 rerendering

--- ui/src/routes/about.lazy.tsx
+++ ui/src/routes/about.lazy.tsx
@@ -1,125 +1,111 @@
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
 import { ReactP5Wrapper, Sketch } from "@p5-wrapper/react";
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
+import { useMemo, useRef, useState } from "react";
 
 export const Route = createLazyFileRoute("/about")({
   component: About,
 });
 
 function About() {
-  const [startRecording, setStartRecording] = useState(false);
-  const sketch: Sketch = (p5) => {
-    let grid: number[][];
-    const cols = 10;
-    const rows = cols;
-    const resolution = 50;
-    const frameRate = 5;
-    let recorder: MediaRecorder;
-    let chunks: Blob[] = [];
-    let isRecording = false;
+  const chunks = useRef<Blob[]>([]);
 
-    p5.setup = () => {
-      p5.createCanvas(500, 500);
-      p5.frameRate(frameRate);
-      grid = createGrid();
-    };
+  const [isRecording, setIsRecording] = useState(true);
 
-    p5.draw = () => {
-      if (p5.frameCount === 1) {
-        isRecording = true;
-
-        recorder = new MediaRecorder(
-          p5.drawingContext.canvas.captureStream(frameRate)
-        );
-
-        recorder.ondataavailable = (e) => {
-          if (e.data.size > 0) {
-            chunks.push(e.data);
-          }
-        };
-
-        recorder.onstop = () => {
-          isRecording = false;
-          const blob = new Blob(chunks, { type: "video/mp4" });
-          const url = URL.createObjectURL(blob);
-          const a = document.createElement("a");
-          document.body.appendChild(a);
-          a.href = url;
-          a.download = "cellular_automata.mp4";
-          a.click();
-          window.URL.revokeObjectURL(url);
-        };
-        recorder.start();
-        return;
-      }
-
-      if (p5.frameCount >= 32) {
-        if (isRecording) stopRecording();
-        p5.noLoop();
-        return;
-      }
-      p5.background(220);
-      for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-          const x = i * resolution;
-          const y = j * resolution;
-          if (grid[j][i] === 1) {
-            p5.fill("orange");
-            p5.stroke(220);
-            p5.text(p5.frameCount - 1, x, y);
-            p5.rect(x, y, resolution - 1, resolution - 1);
-          } else {
-            p5.fill("blue");
-            p5.stroke(220);
-            p5.rect(x, y, resolution - 1, resolution - 1);
-          }
-        }
-      }
-      grid = createGrid();
-    };
-
-    p5.updateWithProps = (props) => {
-      if (props.startRecording) {
-        startRecording();
-      }
-    };
-
-    function startRecording() {
-      chunks = [];
-      isRecording = true;
-      recorder = new MediaRecorder(p5.drawingContext.canvas.captureStream(5), {
-        mimeType: "video/webm",
-      });
-
-      recorder.ondataavailable = (e) => {
-        if (e.data.size > 0) {
-          chunks.push(e.data);
-        }
-      };
-
-      recorder.start();
-    }
-
-    function stopRecording() {
-      recorder.stop();
-
-      setStartRecording(false);
-    }
-
-    function createGrid() {
-      return Array.from({ length: rows }).map(() =>
-        Array.from({ length: cols }).map(() => Math.floor(p5.random([0, 1])))
-      );
-    }
+  const generateVideo = () => {
+    const blob = new Blob(chunks.current, { type: "video/mp4" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = url;
+    a.download = "cellular_automata.mp4";
+    a.click();
+    window.URL.revokeObjectURL(url);
   };
 
-  const saveVideo = () => setStartRecording(true);
+  const memoizedReactP5Wrapper = useMemo(() => {
+    const sketch: Sketch = (p5) => {
+      let grid: number[][];
+      const cols = 100;
+      const rows = cols;
+      const resolution = 10;
+      const frameRate = 5;
+      let recorder: MediaRecorder;
+
+      p5.setup = () => {
+        p5.createCanvas(500, 500);
+        p5.frameRate(frameRate);
+        grid = createGrid();
+      };
+
+      p5.draw = () => {
+        if (p5.frameCount === 1) {
+          recorder = new MediaRecorder(
+            p5.drawingContext.canvas.captureStream(frameRate)
+          );
+
+          recorder.ondataavailable = (e) => {
+            if (e.data.size > 0) {
+              chunks.current.push(e.data);
+            }
+          };
+
+          recorder.onstop = () => {
+            setIsRecording(false);
+          };
+
+          recorder.start();
+          return;
+        }
+
+        if (p5.frameCount >= 32) {
+          if (isRecording) recorder.stop();
+          p5.noLoop();
+          return;
+        }
+
+        p5.background(220);
+
+        for (let i = 0; i < cols; i++) {
+          for (let j = 0; j < rows; j++) {
+            const x = i * resolution;
+            const y = j * resolution;
+            if (grid[j][i] === 1) {
+              p5.fill("orange");
+              p5.stroke(220);
+              p5.rect(x, y, resolution - 1, resolution - 1);
+            } else {
+              p5.fill("blue");
+              p5.stroke(220);
+              p5.rect(x, y, resolution - 1, resolution - 1);
+            }
+          }
+        }
+        grid = createGrid();
+      };
+
+      function createGrid() {
+        return Array.from({ length: rows }).map(() =>
+          Array.from({ length: cols }).map(() => p5.random([0, 1]))
+        );
+      }
+    };
+    return <ReactP5Wrapper sketch={sketch} />;
+  }, []);
 
   return (
-    <div className="flex justify-center">
-      <ReactP5Wrapper sketch={sketch} startRecording={startRecording} />
-      <Button onClick={saveVideo}>Save video</Button>
+    <div className="flex flex-col items-center gap-2">
+      {memoizedReactP5Wrapper}
+      <Button className="w-1/5" disabled={isRecording} onClick={generateVideo}>
+        {isRecording ? (
+          <Spinner size="small">
+            <span className="ml-2">Running simulation...</span>
+          </Spinner>
+        ) : (
+          "Generate mp4 video"
+        )}
+      </Button>
     </div>
   );
 }
