179e6cb39136e0977a96b26af8f5e91e8211f0cb
Alexms95 <silva.alexmatos@outlook.com>
2024-11-02 22:22:52 -0300

    Matriz de J, falta corrigir from B to B

--- ui/src/components/newSimulation.tsx
+++ ui/src/components/newSimulation.tsx
@@ -8,9 +8,11 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import httpClient from "@/lib/httpClient";
 import { colors } from "@/models/colors";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { PlusCircle, TrashIcon } from "lucide-react";
+import { useEffect } from "react";
 import { useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "./ui/button";
@@ -34,7 +36,6 @@ import {
 } from "./ui/select";
 import { Separator } from "./ui/separator";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
-import httpClient from "@/lib/httpClient";
 
 const formSchema = z.object({
   simulationName: z.string().min(3, {
@@ -54,8 +55,30 @@ const formSchema = z.object({
         message: "It must be at least 1 character.",
       }),
       color: z.string(),
+      initialNumber: z.coerce
+        .number({ message: "It must be a number." })
+        .int("It must be an integer number.")
+        .positive("It must be a positive number."),
     })
   ),
+  parameters: z.object({
+    Pm: z.array(
+      z.coerce
+        .number()
+        .gte(0, { message: "It must be greater or equal to 0." })
+        .lte(1, { message: "It must be less or equal to 1." })
+    ),
+    J: z.array(
+      z.object({
+        from: z.string({ message: "It must be a string." }),
+        to: z.string({ message: "It must be a string." }),
+        value: z.coerce
+          .number()
+          .gte(0, { message: "It must be greater or equal to 0." })
+          .lte(1, { message: "It must be less or equal to 1." }),
+      })
+    ),
+  }),
 });
 
 type NewSimulationForm = z.infer<typeof formSchema>;
@@ -68,7 +91,11 @@ export const NewSimulation = () => {
       simulationName: "",
       iterationsNumber: 1,
       gridDimension: 1,
-      ingredients: [{ name: "A", color: "blue" }],
+      ingredients: [{ name: "A", initialNumber: 1, color: "blue" }],
+      parameters: {
+        Pm: [1],
+        J: [{ from: "A", to: "A", value: 1 }],
+      },
     },
   });
 
@@ -77,7 +104,26 @@ export const NewSimulation = () => {
     name: "ingredients",
   });
 
+  const generatePairMatrix = (arraySize: number) => {
+    const result: number[][] = [];
+
+    for (let i = 0; i < arraySize; i++) {
+      for (let j = 0; j < arraySize; j++) {
+        result.push([i, j]);
+      }
+    }
+
+    return result;
+  };
+
+  const pairMatrix = generatePairMatrix(fields.length);
+
+  useEffect(() => {
+    console.log(form.getValues());
+  });
+
   const onSubmit = (values: NewSimulationForm) => {
+    console.log(values);
     httpClient.post("/simulations", values);
   };
 
@@ -178,7 +224,7 @@ export const NewSimulation = () => {
             <Separator />
             <div className="space-y-2">
               {fields.map((field, index) => (
-                <div key={field.id} className="flex space-x-4">
+                <div key={field.id} className="flex space-x-2">
                   <p className="w-1/8 text-sm font-semibold mt-10">
                     Ingredient {String.fromCharCode(65 + index)}
                   </p>
@@ -200,7 +246,27 @@ export const NewSimulation = () => {
                       )}
                     />
                   </div>
-                  <div className="w-1/2">
+                  <div className="w-1/4">
+                    <FormField
+                      control={form.control}
+                      name={`ingredients.${index}.initialNumber`}
+                      defaultValue={1}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Initial number</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="number"
+                              placeholder="Initial count of the ingredient"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="w-1/5">
                     <FormField
                       control={form.control}
                       name={`ingredients.${index}.color`}
@@ -265,9 +331,10 @@ export const NewSimulation = () => {
             </FormMessage>
             <Button
               className="ml-auto py-2 px-3 text-xs"
+              type="button"
               onClick={() =>
                 append({
-                  name: "",
+                  name: String.fromCharCode(65 + fields.length),
                   color: colors.filter(
                     (c) =>
                       !form
@@ -275,14 +342,99 @@ export const NewSimulation = () => {
                         .flatMap((i) => i.color)
                         .includes(c.name)
                   )[0].name,
+                  initialNumber: 1,
                 })
               }
             >
               <PlusCircle className="p-1 pl-0"></PlusCircle>Add Ingredient
             </Button>
             <Separator />
-            <div>
-              <p className="font-semibold">Parameters</p>
+            <h4 className="scroll-m-20 font-semibold tracking-tight">
+              Parameters
+            </h4>
+            <div className="flex space-x-2">
+              {fields.map((field, index) => (
+                <FormField
+                  key={field.id}
+                  control={form.control}
+                  name={`parameters.Pm.${index}`}
+                  defaultValue={1}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>
+                        P<sub>M</sub> ({String.fromCharCode(65 + index)})
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          step={0.1}
+                          min={0}
+                          max={1}
+                          type="number"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              ))}
+            </div>
+            <div className="flex space-x-2">
+              {pairMatrix.map((comb, index) => (
+                <div key={index}>
+                  <FormField
+                    control={form.control}
+                    name={`parameters.J.${index}.from`}
+                    defaultValue={String.fromCharCode(65 + comb[0])}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input className="hidden" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name={`parameters.J.${index}.to`}
+                    defaultValue={String.fromCharCode(65 + comb[1])}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input className="hidden" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name={`parameters.J.${index}.value`}
+                    defaultValue={1}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>
+                          J (
+                          {String.fromCharCode(65 + comb[0]) +
+                            String.fromCharCode(65 + comb[1])}
+                          )
+                        </FormLabel>
+                        <FormControl>
+                          <Input
+                            step={0.1}
+                            min={0}
+                            max={1}
+                            type="number"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              ))}
             </div>
           </form>
           <DialogFooter className="sm:justify-between">
