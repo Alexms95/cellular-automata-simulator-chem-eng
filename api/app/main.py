ec3de739d53ece1bf23963c21ca74bafe1ca445d
Alexms95 <silva.alexmatos@outlook.com>
2024-11-05 22:11:55 -0300

    Remove calculation router from main application

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.views.calculation_view import router as calculation_router
 from app.views.simulation_view import router as simulation_router
 
 app = FastAPI(title="Cellular Automata Calculator API")
@@ -15,7 +14,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(calculation_router, prefix="/calculate", tags=["calculation"])
 app.include_router(simulation_router, prefix="/simulations", tags=["Simulations"])
 
 @app.get("/")
