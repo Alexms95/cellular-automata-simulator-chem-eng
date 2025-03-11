452ff8852bfbddd926d4b80afbe831c9cdb973e4
Alex Silva <alex.silva@radixeng.com.br>
2025-03-11 19:28:42 -0300

    Add probability to the execution of Pm analysis and update axios to fix a vulnerability

--- api/calculations.py
+++ api/calculations.py
@@ -28,8 +28,7 @@ def calculate_cell_counts(total: int, percentages: list[float]) -> list[int]:
     return rounded_counts
 
 
-def calculate_cellular_automata(simulation: SimulationBase) -> np.matrix:
-
+def calculate_cellular_automata(simulation: SimulationBase) -> np.ndarray:
     NL = simulation.gridHeight
     NC = simulation.gridLenght
 
@@ -42,17 +41,18 @@ def calculate_cellular_automata(simulation: SimulationBase) -> np.matrix:
     NEMPTY = floor(EMPTY_FRAC * NTOT)
     NCELL = NTOT - NEMPTY
 
-    ingredients = simulation.ingredients
+    components = simulation.ingredients
+    parameters = simulation.parameters
 
-    NCOMP = len(ingredients)
+    NCOMP = len(components)
 
     print(NCOMP)
 
-    NOME_COMP = [ingredient.name for ingredient in ingredients]
+    NOME_COMP = [component.name for component in components]
 
     print(NOME_COMP)
 
-    Ci = np.array([ing.molarFraction for ing in ingredients])
+    Ci = np.array([comp.molarFraction for comp in components])
 
     print(Ci)
 
@@ -61,7 +61,18 @@ def calculate_cellular_automata(simulation: SimulationBase) -> np.matrix:
     print(Ni)
 
     # Initial matrix
-    M = np.matrix(np.zeros((NL, NC)), dtype=int)
+    # matrix = [
+    #     [0, 2, 0, 2, 1, 2, 1, 3, 1, 3],
+    #     [3, 0, 0, 0, 3, 0, 3, 0, 0, 1],
+    #     [0, 0, 3, 2, 1, 1, 1, 1, 1, 1],
+    #     [2, 0, 0, 3, 2, 0, 3, 0, 2, 0],
+    #     [0, 0, 1, 0, 2, 3, 3, 2, 2, 3],
+    #     [2, 2, 3, 3, 1, 0, 2, 2, 3, 0],
+    #     [2, 3, 3, 3, 1, 0, 0, 0, 1, 3],
+    #     [0, 1, 1, 3, 2, 3, 1, 3, 3, 2],
+    # ]
+    # M = np.array(matrix)
+    M = np.zeros((NL, NC), dtype=int)
     # Randomly distribute the components in the matrix
     for i in range(NCOMP):
         for j in range(Ni[i]):
@@ -71,20 +82,49 @@ def calculate_cellular_automata(simulation: SimulationBase) -> np.matrix:
                 if M[r, c] == 0:
                     M[r, c] = i + 1
                     break
-                
+
     # Show the matrix formatting the output as a table
     print("Initial matrix:")
     show_matrix(M)
-  
-    # Define the neighborhood
-    NEIGH = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+    # Define the Von Neumann neighborhood
+    NEIGH = np.array([[-1, 0], [1, 0], [0, -1], [0, 1]], dtype=int)
+
+    # Shuffle the neighborhood
+    np.random.shuffle(NEIGH)
 
     # Define the number of iterations
     n_iter = simulation.iterationsNumber
 
+    moved_components = set()
+
+    for n in range(n_iter):
+        M_new = M.copy()
+        moved_components.clear()
+        for i in range(NL):
+            for j in range(NC):
+                i_comp = M[i, j]
+                if i_comp > 0 and (i, j) not in moved_components:
+                    for n in range(len(NEIGH)):
+                        r = i + NEIGH[n, 0]
+                        c = j + NEIGH[n, 1]
+                        if r >= 0 and r < NL and c >= 0 and c < NC:
+                            if M_new[r, c] == 0:
+                                pm_component = parameters.Pm[i_comp - 1]
+                                if maybe_execute(pm_component):
+                                    M_new[r, c] = i_comp
+                                    M_new[i, j] = 0
+                                    moved_components.add((r, c))
+                                    np.random.shuffle(NEIGH)
+                                    break
+        M = M_new.copy()
+
+    print("Final matrix:")
+    show_matrix(M)
     return M
 
-def show_matrix(M):
+
+def show_matrix(M: np.ndarray):
     NL, NC = M.shape
     for i in range(NL):
         for j in range(NC):
@@ -92,4 +132,5 @@ def show_matrix(M):
         print()
     print()
 
-    
+def maybe_execute(probability: float) -> bool:
+    return np.random.random() < probability
