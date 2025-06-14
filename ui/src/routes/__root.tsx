4e0584163730836fb8a77d93e7fd6d4c9ce88929
Alexms95 <silva.alexmatos@gmail.com>
2025-06-14 18:29:59 -0300

    refactor: Update dark mode background color for improved contrast

--- ui/src/routes/__root.tsx
+++ ui/src/routes/__root.tsx
@@ -8,7 +8,7 @@ import {
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="min-h-screen p-8 bg-zinc-100 dark:bg-zinc-900">
+    <div className="min-h-screen p-8 bg-zinc-100 dark:bg-zinc-950">
       <Outlet />
       <footer className="fixed bottom-0 left-0 w-full bg-zinc-800 text-white dark:text-zinc-900 dark:bg-white text-xs text-center p-2 flex items-center justify-evenly">
         <p>&copy; {new Date().getFullYear()} Cellular Automata Simulator</p>
